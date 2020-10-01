1bd24afac86ff94ddc2f9972459315f0ce36a21d
DebailleuxJonathan <jonathan.debailleux@edu.itescia.fr>
2020-10-01 12:51:11 +0200

    US done

--- src/main.js
+++ src/main.js
@@ -5,17 +5,25 @@ import router from '@/router'
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default.css'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import { BootstrapVue, IconsPlugin, BootstrapVueIcons } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'bootstrap-vue/dist/bootstrap-vue-icons.min.css'
 import VueMoment from 'vue-moment'
 import AsyncComputed from 'vue-async-computed'
+import momentTimezone from 'moment-timezone'
+import VSwitch from 'v-switch-case'
+
+const moment = require('moment')
+require('moment/locale/fr')
 
 Vue.config.productionTip = false
 Vue.use(VueMaterial)
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
-Vue.use(VueMoment)
+Vue.use(BootstrapVueIcons)
+Vue.use(VueMoment, { moment, momentTimezone })
+Vue.use(VSwitch)
 Vue.use(AsyncComputed)
 
 new Vue({
